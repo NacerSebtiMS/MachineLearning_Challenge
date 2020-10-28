cf8cedda4872f2d25094ada464033224a4c43642
Nacer Sebti <nacersebti.ms@gmail.com>
2020-10-28 19:18:30 -0400

    Data format

---
+++ Nacer_Sebti_MachineLearning_Script.R
@@ -0,0 +1,16 @@
+getwd()
+
+job_skills = read.csv("job_skills.csv")
+summary(job_skills)
+
+lf_sex_age = read.csv("labour_force_sex_age.csv")
+emp_sex_age = read.csv("employment_sex_age.csv")
+
+lf_industry = read.csv("labour_force_industry.csv")
+emp_industry = read.csv("employment_industry.csv")
+
+lf_province = read.csv("labour_force_province.csv")
+emp_province = read.csv("employment_province.csv")
+
+lf_ethnicity = read.csv("labour_force_ethnicity.csv")
+emp_ethnicity = read.csv("employment_ethnicity.csv")
